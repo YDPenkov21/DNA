b37ab0470cbea73c672d0c829c19d5188215e142
Bozhidar Dimov <BADimov21@codingburgas.bg>
2023-10-30 16:31:47 +0200

    Add avatar mechanisms

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,23 +1,54 @@
 ﻿#include <iostream>
 #include "raylib.h"
 
+class Bird {
+public:
+    float x, y;
+    int speedX;
+    int radius;
+
+    void draw() {
+        DrawCircle(x, y, radius, WHITE);
+    }
+    
+    void update() {
+        if (IsKeyDown(KEY_LEFT)) {
+            x -= speedX;
+        }
+        else if (IsKeyDown(KEY_RIGHT)) {
+            x += speedX;
+        }
+        if (x - radius <= 0) {
+            x = radius;
+        }
+        else if (x + radius >= GetScreenWidth()) {
+            x = GetScreenWidth() - radius;
+        }
+    }
+};
+
+Bird bird;
+
 int main()
 {
-    const int screenWidth = 800;
-    const int screenHeight = 450;
+    
+    const int screenWidth = 1920;
+    const int screenHeight = 975;
 
-    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+    bird.x = screenWidth / 2;
+    bird.y = 900;
+    bird.speedX = 10;
+    bird.radius = 20;
+    InitWindow(screenWidth, screenHeight, "Flappy Quiz");
 
     SetTargetFPS(60);
 
     while (!WindowShouldClose())
     {
         BeginDrawing();
-
-        ClearBackground(RAYWHITE);
-
-        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
-
+        bird.update();
+        ClearBackground(BLACK);
+        bird.draw();
         EndDrawing();
     }
     CloseWindow();
