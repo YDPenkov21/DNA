4c83831539b73314693a6ff2fae919db18816786
Bozhidar Dimov <BADimov21@codingburgas.bg>
2023-10-31 15:40:38 +0200

    Add rain mechanism and random spawn
    Co-authored-by: Bozhidar Dimov <BADimov21@codingburgas.bg>
    Co-authored-by: Gospodin Radev <GSRadev21@codingburgas.bg>

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+﻿#include <vector>
 #include "raylib.h"
 
 class Bird {
@@ -10,7 +10,7 @@ public:
     void draw() {
         DrawCircle(x, y, radius, WHITE);
     }
-    
+
     void update() {
         if (IsKeyDown(KEY_LEFT)) {
             x -= speedX;
@@ -27,30 +27,64 @@ public:
     }
 };
 
+const int maxRaindrops = 60;
+
+class Raindrop {
+public:
+    Vector2 position;
+    Color color;
+    float speed;
+};
+
 Bird bird;
 
-int main()
-{
-    
+int main() {
     const int screenWidth = 1920;
     const int screenHeight = 975;
 
     bird.x = screenWidth / 2;
-    bird.y = 900;
+    bird.y = screenHeight - 75;
     bird.speedX = 10;
     bird.radius = 20;
-    InitWindow(screenWidth, screenHeight, "Flappy Quiz");
 
+    InitWindow(screenWidth, screenHeight, "Flappy Quiz");
     SetTargetFPS(60);
 
-    while (!WindowShouldClose())
-    {
-        BeginDrawing();
+    std::vector<Raindrop> raindrops;
+
+    for (int i = 0; i < maxRaindrops; i++) {
+        Raindrop raindrop;
+        raindrop.position.x = GetRandomValue(0, screenWidth);
+        raindrop.position.y = GetRandomValue(0, screenHeight);
+        raindrop.color = YELLOW;
+        raindrop.speed = GetRandomValue(12, 15) * 0.1;
+        raindrops.push_back(raindrop);
+    }
+
+    while (!WindowShouldClose()) {
+
         bird.update();
+
+        BeginDrawing();
         ClearBackground(BLACK);
+
+        for (size_t i = 0; i < raindrops.size(); i++) {
+            raindrops[i].position.y += raindrops[i].speed;
+
+            if (raindrops[i].position.y > screenHeight) {
+                raindrops[i].position.y = 0;
+                raindrops[i].position.x = GetRandomValue(0, screenWidth);
+            }
+
+            DrawCircleV(raindrops[i].position, 5, raindrops[i].color);
+        }
+
         bird.draw();
+
         EndDrawing();
     }
+
     CloseWindow();
+
     return 0;
 }
